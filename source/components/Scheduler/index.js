edda3f3c34dd35fd6151726a6fde11f9cd2b812c
Artem Slepets <49h4DhPERFECT@gmail.com>
2018-05-10 11:20:44 +0300

    Basic Scheduler component created

--- source/components/Scheduler/index.js
+++ source/components/Scheduler/index.js
@@ -3,13 +3,104 @@ import React from "react";
 
 // Instruments
 import Styles from "./styles.m.css";
+import moment from 'moment';
 
 // Components
 import Task from 'components/Task';
 import Checkbox from 'theme/assets/Checkbox';
 
 class Scheduler extends React.Component {
+    constructor () {
+        super();
+        this.state = {
+            tasks: [
+                {
+                    "id":        "xpa",
+                    "message":   "Stop being such retard",
+                    "completed": false,
+                    "favorite":  true,
+                },
+                {
+                    "id":        "xjh",
+                    "message":   "Успешно пройти React-интенсив компании Lectrum",
+                    "completed": false,
+                    "favorite":  true,
+                },
+                {
+                    "id":        "xjr",
+                    "message":   "Взять автограф у Джареда Лето",
+                    "completed": false,
+                    "favorite":  false,
+                },
+                {
+                    "id":        "xrh",
+                    "message":   "Зарегестрировать бабушку в Твиче",
+                    "completed": false,
+                    "favorite":  false,
+                },
+                {
+                    "id":        "rjh",
+                    "message":   "Записать собаку на груминг",
+                    "completed": false,
+                    "favorite":  false,
+                },
+                {
+                    "id":        "xph",
+                    "message":   "Научиться играть на барабанах",
+                    "completed": true,
+                    "favorite":  false,
+                }
+            ],
+            taskDescription: '',
+        };
+        this.onAddTaskHandler = this._onAddTaskHandler.bind(this);
+        this.onInputChangeHandler = this._onInputChangeHandler.bind(this);
+        this.onSendTaskHandler = this._onSendTaskHandler.bind(this);
+    }
+
+    _onAddTaskHandler (event) {
+        const {
+            taskDescription,
+        } = this.state;
+        const currentTimeStamp = moment().format("MMMM D h:mm:ss a");
+
+        event.preventDefault();
+        if (taskDescription) {
+            this.setState(({ tasks }) => ({
+                tasks: [{
+                    id:        '123ss'+Math.random(),
+                    message:   taskDescription,
+                    completed: false,
+                    favorite:  false,
+                    created:   currentTimeStamp,
+                    modified:  currentTimeStamp,
+                }, ...tasks],
+                taskDescription: '',
+            }), () => {
+                console.log(this.state.tasks);
+            });
+        }
+    }
+
+    _onInputChangeHandler ({ target: { value }}) {
+        this.setState({
+            // id,
+            taskDescription: value,
+        });
+    }
+
+    _onSendTaskHandler (event) {
+        console.log(event.nativeEvent.keyCode);
+    }
+
     render () {
+        const {
+            tasks,
+            taskDescription,
+        } = this.state;
+
+        const renderTasks = tasks.map((task) => <Task key = { task.id } { ...task } />);
+
         return (
             <section className = { Styles.scheduler }>
                 <main>
@@ -19,12 +110,18 @@ class Scheduler extends React.Component {
                     </header>
                     <section>
                         <form>
-                            <input placeholder = 'Описание моей новой задачи' type = 'text' />
-                            <button>Добавить задачу</button>
+                            <input
+                                maxLength = { 50 }
+                                placeholder = 'Описание моей новой задачи'
+                                type = 'text' value = { taskDescription }
+                                onChange = { this.onInputChangeHandler }
+                                onKeyPress = { this.onSendTaskHandler }
+                            />
+                            <button onClick = { this.onAddTaskHandler }>Добавить задачу</button>
                         </form>
                         <div className = { Styles.overlay } >
                             <ul>
-                                <Task />
+                                {renderTasks}
                             </ul>
                         </div>
                     </section>
@@ -35,7 +132,7 @@ class Scheduler extends React.Component {
                             color1 = '#363636'
                             color2 = '#fff'
                         />
-                        <span className = { Styles.completeAllTasks } >Все задачи выполнены</span>
+                        <span className = { Styles.completeAllTasks }>Все задачи выполнены</span>
                     </footer>
                 </main>
             </section>
